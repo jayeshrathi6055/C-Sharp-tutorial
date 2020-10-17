6394173af5572e5a2a817ccb9a11e636c56dd293
jayeshrathi6055 <65055605+jayeshrathi6055@users.noreply.github.com>
2020-10-17 14:06:00 +0530

    Add files via upload

---
+++ player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HELLO_C_SHARP
+{
+    class player
+    {
+        public string name = "J";
+        public int age = 18;
+        public void setage(int a)
+        {
+            age = a;
+        }
+    }
+}
